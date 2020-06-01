302ea08be89d9c93f6a109ef58b7ecd5fb4d4a74
zodiaVG <zhangzibo418@outlook.com>
2020-06-01 12:28:52 +0800

    增加了教师，写完了添加教师界面，删除了数据库的约束（因为解决不了。。）

--- mysite2/view.py
+++ mysite2/view.py
@@ -3,44 +3,94 @@ import pymysql
 
 
 def show_student(request):
-    db = pymysql.connect("localhost", "testuser", "PlayStation5", "dbsclab2020",port=3306,cursorclass=pymysql.cursors.DictCursor)
-    cursor=db.cursor()
-    ctx={}
-    if request.POST:
-        name=request.POST['name']
-        sql="select * from student where name like %s"
-        cursor.execute(sql,['%'+name+'%'])
-    else:
-        sql="select * from student"
-        cursor.execute(sql)
-    data=cursor.fetchall()
-    context={}
-    context['students']=data
-    return render(request,'student.html',context)
-
-
-def addStudent(request):
-    db = pymysql.connect("localhost", "testuser", "PlayStation5", "dbsclab2020", port=3306, cursorclass=pymysql.cursors.DictCursor)
+    db = pymysql.connect("localhost", "testuser", "PlayStation5", "dbsclab2020", port=3306,
+                         cursorclass=pymysql.cursors.DictCursor)
     cursor = db.cursor()
     ctx = {}
     if request.POST:
-        ID=request.POST['ID']
-        name=request.POST['name']
-        dept_name=request.POST['dept_name']
-        tot_cred=request.POST['tot_cred']
-        sql="insert into student values (%s,%s,%s,%s)"
-        cursor.execute(sql,[ID,name,dept_name,tot_cred])
-        db.commit()
-        sql2="select * from student where ID=%s"
-        cursor.execute(sql2,ID)
-        data=cursor.fetchall()
-        ctx['students']=data
+        name = request.POST['name']
+        sql = "select * from student where name like %s"
+        cursor.execute(sql, ['%' + name + '%'])
+    else:
+        sql = "select * from student"
+        cursor.execute(sql)
+    data = cursor.fetchall()
+    context = {}
+    context['students'] = data
+    return render(request, 'student.html', context)
 
-    return render(request, 'addStudent.html',ctx)
+
+def addStudent(request):
+    db = pymysql.connect("localhost", "testuser", "PlayStation5", "dbsclab2020", port=3306,
+                         cursorclass=pymysql.cursors.DictCursor)
+    cursor = db.cursor()
+    ctx = {}
+    if request.POST:
+        ID = request.POST['ID']
+        name = request.POST['name']
+        dept_name = request.POST['dept_name']
+        tot_cred = request.POST['tot_cred']
+        sql = "insert into student values (%s,%s,%s,%s)"
+        cursor.execute(sql, [ID, name, dept_name, tot_cred])
+        db.commit()
+        sql2 = "select * from student where ID=%s"
+        cursor.execute(sql2, ID)
+        data = cursor.fetchall()
+        ctx['students'] = data
+
+    return render(request, 'addStudent.html', ctx)
 
 
 def modifyStudent(request, id):
     return render(request, 'modifyStudent.html')
 
+
 def deleteStudent(request, id):
     return redirect('/search-student')
+
+
+def show_instructor(request):
+    db = pymysql.connect("localhost", "testuser", "PlayStation5", "dbsclab2020", port=3306,
+                         cursorclass=pymysql.cursors.DictCursor)
+    cursor = db.cursor()
+    ctx = {}
+    if request.POST:
+        name = request.POST['name']
+        sql = "select * from instructor where name like %s"
+        cursor.execute(sql, ['%' + name + '%'])
+    else:
+        sql = "select * from instructor"
+        cursor.execute(sql)
+    data = cursor.fetchall()
+    context = {}
+    context['instructors'] = data
+    return render(request, 'instructors.html', context)
+
+
+def addInstructor(request):
+    db = pymysql.connect("localhost", "testuser", "PlayStation5", "dbsclab2020", port=3306,
+                         cursorclass=pymysql.cursors.DictCursor)
+    cursor = db.cursor()
+    ctx = {}
+    if request.POST:
+        ID = request.POST['ID']
+        name = request.POST['name']
+        dept_name = request.POST['dept_name']
+        salary = request.POST['salary']
+        sql = "insert into instructor values (%s,%s,%s,%s)"
+        cursor.execute(sql, [ID, name, dept_name, salary])
+        db.commit()
+        sql2 = "select * from instructor where ID=%s"
+        cursor.execute(sql2, ID)
+        data = cursor.fetchall()
+        ctx['instructors'] = data
+
+    return render(request, 'addInstructor.html', ctx)
+
+
+def modifyInstructor(request, id):
+    return render(request, 'modifyInstructor.html')
+
+
+def deleteInstructor(request, id):
+    return redirect('/search-instructor')
